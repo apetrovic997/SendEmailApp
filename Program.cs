545e62b70b19bc4e55ab78256714899d93e5a104
Andrija Petrovic <73284789+apetrovic997@users.noreply.github.com>
2025-08-12 20:22:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var receiver = configuration["Email:To"];
+        var subject = configuration["Email:Subject"];
+        var emailBody = configuration["Email:EmailBody"];
+
+        //var email = Environment.GetEnvironmentVariable("EMAIL_USERNAME");
+        //var password = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
+        //var smtpHost = Environment.GetEnvironmentVariable("EMAIL_SMTP");
+
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Andrija Petrovic", "an.petrovic1997@gmail.com"));
+        message.To.Add(new MailboxAddress("KOMUNALNA MILICIJA", receiver));
+        message.Subject = subject;
+
+        var builder = new BodyBuilder
+        {
+            TextBody = emailBody
+        };
+
+        builder.Attachments.Add("Andrija_Petrovic_Resume.pdf");
+
+        message.Body = builder.ToMessageBody();
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync("smtp.gmail.com", 587, false);
+        await client.AuthenticateAsync("an.petrovic1997@gmail.com", "dquz mmot tjux vtme");
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+
+        Console.WriteLine("Email je uspešno poslat!");
+    }
+}
